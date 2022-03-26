b564199e3118e76e2cbe14883543bb2910a6002a
Alton <delucaa@wit.edu>
2022-03-25 21:31:02 -0400

    fixed currency exchange page http request, formatted response to be used later

--- src/Components/CurrencyPage/CurrencyPage.js
+++ src/Components/CurrencyPage/CurrencyPage.js
@@ -3,14 +3,28 @@ import { useState, useEffect } from "react";
 
 function CurrencyPage(props) {
   const [data, setData] = useState(0);
+  let todaysDate = new Date(Date.now());
+  let todaysDate_str = todaysDate.toISOString().slice(0, 10);
+  let oneMonthAgo = new Date(todaysDate.setDate(todaysDate.getDate() - 30));
+  let oneMonthAgo_str = oneMonthAgo.toISOString().slice(0, 10);
+  let requestString =
+    "https://api.exchangerate.host/timeseries?start_date=" +
+    oneMonthAgo_str +
+    "&end_date=" +
+    todaysDate_str +
+    "&base=USD";
+
   async function getCurrencyData() {
-    const response = await fetch(
-      "https://api.exchangerate.host/timeseries?start_date=2020-01-01&end_date=2020-01-04"
-    );
+    console.log(todaysDate_str);
+    console.log(oneMonthAgo_str);
+    console.log(requestString);
+
+    const response = await fetch(requestString);
     const theData = await response.json();
+    console.log(theData);
     // let USD = theData["rates"]["USD"];
-    console.log("USD : " + theData["rates"]["2020-01-01"]["USD"]);
-    //setData(USD);
+    console.log("EUR : " + theData["rates"][todaysDate_str]["EUR"]);
+    setData(theData["rates"][todaysDate_str]["EUR"]);
 
     // console.log(data);
     // console.log(data.rates);
@@ -19,7 +33,7 @@ function CurrencyPage(props) {
 
   return (
     <div className="center">
-      <p>EUR to USD : {data}</p>
+      <p>USD to EUR {data}</p>
       <Button onClick={getCurrencyData} content="GET RATES"></Button>
     </div>
   );
